9fd418a4a5c25668e5052607d551aa40954d588e
Sandyarathi Das <sandyarathi1212@gmail.com>
2016-03-07 02:20:36 -0500

    Working Code

--- DCacheJoinDictionaryMapper.java
+++ DCacheJoinDictionaryMapper.java
@@ -19,31 +19,35 @@ import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 
 
 
-public class DCacheJoinDictionaryMapper  extends Mapper<LongWritable, Text, Text, Text> {
+public class DCacheJoinDictionaryMapper  extends Mapper<Text, Text, Text, Text> {
  	String fileName=null, language=null;
 	   public Map<String, String> translations = new HashMap<String, String>();
-	  
-	      
+	  private BufferedReader brReader;
+	   @SuppressWarnings("deprecation")   
 	   public void setup(Context context) throws IOException, InterruptedException{
 		// TODO: determine the name of the additional language based on the file name  
-		   Configuration conf = context.getConfiguration();
-		   Path[] dataFile = DistributedCache.getLocalCacheFiles(conf);
+		try{   
+		   Path[] dataFile = new Path[0];
+		   dataFile = context.getLocalCacheFiles();
 		   for (Path eachPath : dataFile) {
 			   fileName = eachPath.getName().toString().trim();
 			   System.out.println("fileName : "+fileName);
 			   language = fileName.substring(0,fileName.length()-4);
 			   System.out.println("language: "+ language);
-			   createLanguageFileHashMap(eachPath, context);
+			   createLanguageFileHashMap(eachPath);
 		   }
+		} catch(IOException e){
+			System.err.println("Exception reading cache file:" +e);
+		}
 		   
 		   
            
 	    // TODO: OPTIONAL: depends on your implementation -- create a HashMap of translations (word, part of speech, translations) from output of exercise 1
            
 	   }
-	   private void createLanguageFileHashMap(Path filePath, Context context) throws IOException {
-		   BufferedReader brReader;
-		   String strLineRead = "";
+	   private void createLanguageFileHashMap(Path filePath) throws IOException {
+		  try{
+		   String strLineRead = null;
 		   brReader = new BufferedReader(new FileReader(filePath.toString()));
 		   while ((strLineRead = brReader.readLine()) != null) {
 			   if(!(strLineRead.toString().charAt(0)=='#') && (strLineRead.toString().indexOf('['))>0){
@@ -52,50 +56,47 @@ public class DCacheJoinDictionaryMapper  extends Mapper<LongWritable, Text, Text
 					System.out.println("length:"+lineSplit.length);   
 					String englishWord = lineSplit[0];
 				        String[] splitValue=lineSplit[1].toString().split("\\[");
-	                    if(splitValue.length!=0 && splitValue.length!=1 ){
-	                    	String partsOfSpeech = splitValue[1].substring(0,splitValue[1].length()-1);
-	                    	if(valid(partsOfSpeech)){
-	                    		String key= englishWord + " : ["+partsOfSpeech+"]";
-	                            String value = language+ ":"+splitValue[0];
-	                            translations.put(key, value);
-	                    	}
-	                    }
-				   }
+	                   	        if(splitValue.length!=0 && splitValue.length!=1 ){
+	                    			String partsOfSpeech = splitValue[1].substring(0,splitValue[1].length()-1);
+	                    			if(valid(partsOfSpeech)){
+	                    				String key= englishWord + " : ["+partsOfSpeech+"]";
+	                           			 String value = language+ ":"+splitValue[0];
+	                           			System.out.println("key: "+key);
+							System.out.println("value: "+value);
+							 translations.put(key, value);
+	                    			}
+	                   		 }
+		 		 }
 			   }
 		   }
+		} catch (IOException ioe){
+			System.err.println("Exception while reading cache file!"+ ioe.toString());
+		}
 		  
-	   brReader.close();
+		 brReader.close();
 	}
-		
+			
    private boolean valid(String partsOfSpeech) {
 		String[] words = {"Noun", "Pronoun", "Verb", "Adverb", "Adjective", "Preposition", "Article", "Conjunction"};  
 	    return (Arrays.asList(words).contains(partsOfSpeech));
 	}
+	
 	public void map(Text key, Text value, Context context) throws IOException, InterruptedException {
 		// TODO: perform a map-side join between the word/part-of-speech from exercise 1 and the word/part-of-speech from the distributed cache file
-		String appendValue=null;	
-	       	if (translations.containsKey(key)){
-			appendValue= value.toString()+"|"+translations.get(key);
-			}
-			else{
-				appendValue=language+":N/A";
-			}
-		/*Configuration conf = context.getConfiguration();
-		Path[] dataFile = DistributedCache.getLocalCacheFiles(conf);
-		for(Path path : dataFile){
-			if(dataFile.toString().endsWith("latin.txt")){
-				BufferedReader brReader;
-                   		String strLineRead = "";
-                   		brReader = new BufferedReader(new FileReader(path.toString()));
-                   		while ((strLineRead = brReader.readLine()) != null) {
-					context.write(new Text("LATIN"),new Text(strLineRead));
-				}
-			}
+		String appendValue=null;
+		System.out.println("Searching for key: "+key);
+		System.out.println("And value: "+value);	
+	       	if (translations.containsKey(key.toString())){
+			System.out.println("match");
+			appendValue= value.toString()+" | "+translations.get(key.toString());
+			System.out.println("append Value"+appendValue);
+
 		}
-			for (String keyHash: translations.keySet()){
-			context.write(new Text("null"), new Text("null"));*/
-			context.write(new Text(key), new Text(appendValue));
-		}
+		else{
+			appendValue=value.toString()+" | "+language+":N/A";
+		}	
+		context.write(new Text(key), new Text(appendValue));
+			
 	      }
 
    }
